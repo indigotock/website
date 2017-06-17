a7037dde13eaa851887bf96135ced5764e669e46
Kyle Hughes <kyle@kyle-2.local>
2017-06-17 18:07:45 +0100

    sync

--- ac/src/index.js
+++
@@ -1,7 +0,0 @@
-import { Engine } from './Engine'
-import TestScene from './scenes/TestScene'
-
-let e = new Engine(document.getElementById('container'));
-e.setScene(TestScene)
-e.camera.position.set(0, 10, 100)
-e.start()
---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        path: __dirname,
+        filename: "bundle.js"
+    },
+    resolve: {
+        extensions: ['.ts', '.css', '.js', '.ne']
+    },
+    module: {
+        loaders: [
+            { test: /\.ne$/, loader: 'nearley-loader' },
+            {
+                test: /\.s?css$/,
+                loader: "style-loader!css-loader!postcss-loader!sass-loader"
+            },
+            { test: /\.ts$/, loader: "ts-loader" }
+        ]
+    }
+};
