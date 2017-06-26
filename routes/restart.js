3552ebe92671d7ea8c1d1236207210982b08eb7c
Kyle Hughes <kyle@kyle-2.local>
2017-06-26 05:12:55 +0100

    add process kill

--- routes/restart.js
+++ routes/restart.js
@@ -2,15 +2,20 @@ var express = require('express');
 var router = express.Router();
 var path = require('path');
 
-
 router.get('/', function (req, res, next) {
   res.sendFile(path.join(__dirname, '..', 'public', 'restart.html'))
 })
 
+
+
 router.post('/', function (req, res, next) {
   if (req.body.pass && req.body.pass == require('../restartCode')) {
     res.write('Confirmed', (e) => {
+      console.log(require('child_process'))
       res.end();
+      require('child_process').exec('pkill -n nginx', function (e, o, se) {
+        console.log('Attempted reboot', e, o, se)
+      });
     })
   } else {
     res.sendStatus(403)
