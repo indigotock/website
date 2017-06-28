b6e3cf80bec56b01f45e8f9d1786a9ecb72e4301
Kyle Robert Hughes <kyle.hughes@outlook.com>
2017-06-28 02:26:43 +0100

    add cache clearing on webook

--- routes/elemeno.js
+++ routes/elemeno.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
 var path = require('path');
+var cms = require('../cms');
 
 router.post('/', function (req, res, next) {
-  console.log(req.body);
+  cms.clearCache();
+  res.sendStatus(200);
 });
 
 module.exports = router;
