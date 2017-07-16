05ca232f556aa81731ac9236368174f62989fd7d
Kyle Robert Hughes <kyle.hughes@outlook.com>
2017-07-16 17:12:38 +0100

    add sass and ts building also gitignore

--- client/gulpfile.js
+++ client/gulpfile.js
@@ -1,8 +1,54 @@
 let gulp = require('gulp')
+let browserify = require("browserify");
+let source = require('vinyl-source-stream');
+let watchify = require("watchify");
+let tsify = require("tsify");
+let gutil = require("gulp-util");
+let sass = require('gulp-sass')
+let concat = require('gulp-concat')
+let postcss = require('gulp-postcss')
+var autoprefixer = require('autoprefixer');
+let cssnano = require('cssnano');
 
+let base_typescript = browserify({
+    basedir: '.',
+    entries: ['ts/index.ts']
+}).plugin(tsify)
 
-gulp.task('build', function () {
+function build_typescript() {
+    return base_typescript.bundle().pipe(source('index.js'))
+        .pipe(gulp.dest('js'))
+}
 
+function watch_typescript() {
+    let watch = watchify(base_typescript)
+    watch.on('update', build_typescript)
+    watch.on('log', gutil.log)
+    return watch.bundle().pipe(source('index.js'))
+        .pipe(gulp.dest('js'))
+}
+
+function build_sass() {
+    gulp.src('sass/**/*.scss')
+        .pipe(sass().on('error', gutil.log))
+        .pipe(concat('style.css'))
+        .pipe(postcss([autoprefixer(), cssnano()], {}))
+        .pipe(gulp.dest('css'))
+}
+
+gulp.task('sass', build_sass)
+gulp.task('watch_sass', function () {
+    gulp.watch('sass/**/*.scss', ['sass'])
 })
 
-gulp.task('default', ['build'])
+gulp.task('typescript', build_typescript)
+gulp.task('watch_typescript', watch_typescript)
+
+
+gulp.task('build', ['sass', 'typescript'])
+gulp.task('watch', ['watch_sass', 'watch_typescript'])
+gulp.task('default', ['watch'])
+
+gulp.task('minify', ['build'], function () {
+
+})
---
+++ client/ts/index.ts
@@ -0,0 +1,7 @@
+import imp from './test'
+
+let debug = function (inp: string) {
+    console.log(inp)
+}
+
+debug('Typescript test25asdda' + imp)
