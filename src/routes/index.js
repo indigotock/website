3cac6a81d14cf5883debc840435227d0069fa0d6
Kyle Robert Hughes <kyle.hughes@outlook.com>
2017-07-22 23:32:01 +0100

    Damn. This should do it

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var debug = require('debug')('site:routes')
 /* GET home page. */
-router.get('/', async function (req, res, next) {
+router.get('/', function (req, res, next) {
     res.render('index', {
         projects: require('../projects'),
     })
